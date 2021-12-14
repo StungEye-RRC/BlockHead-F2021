7ebedf0f958c0774a0244a02e8b0d77d390c0d86
Kyle Geske <stungeye@gmail.com>
2021-12-14 11:51:09 -0600

    Added the static mesh component to the BHPawn.

--- Source/BlockHead/Private/BHPawn.cpp
+++ Source/BlockHead/Private/BHPawn.cpp
@@ -3,32 +3,31 @@
 
 #include "BHPawn.h"
 
+#include "Engine/StaticMeshSocket.h"
+
 // Sets default values
-ABHPawn::ABHPawn()
-{
- 	// Set this pawn to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+ABHPawn::ABHPawn() {
+	// Set this pawn to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
+	Cube = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("Cube"));
+	RootComponent = Cube;
 
 }
 
 // Called when the game starts or when spawned
-void ABHPawn::BeginPlay()
-{
+void ABHPawn::BeginPlay() {
 	Super::BeginPlay();
-	
+
 }
 
 // Called every frame
-void ABHPawn::Tick(float DeltaTime)
-{
+void ABHPawn::Tick(float DeltaTime) {
 	Super::Tick(DeltaTime);
 
 }
 
 // Called to bind functionality to input
-void ABHPawn::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
-{
+void ABHPawn::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent) {
 	Super::SetupPlayerInputComponent(PlayerInputComponent);
 
 }
-
--- Source/BlockHead/Public/BHPawn.h
+++ Source/BlockHead/Public/BHPawn.h
@@ -7,23 +7,25 @@
 #include "BHPawn.generated.h"
 
 UCLASS()
-class BLOCKHEAD_API ABHPawn : public APawn
-{
+class BLOCKHEAD_API ABHPawn : public APawn {
 	GENERATED_BODY()
 
 public:
 	// Sets default values for this pawn's properties
 	ABHPawn();
 
-protected:
-	// Called when the game starts or when spawned
-	virtual void BeginPlay() override;
-
-public:	
 	// Called every frame
 	virtual void Tick(float DeltaTime) override;
 
 	// Called to bind functionality to input
 	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Setup")
+	UStaticMeshComponent* Cube;
+
 
 };
