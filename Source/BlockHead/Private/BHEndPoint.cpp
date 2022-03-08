1e23ca445d1bd9528467a872ffa35b1a470ae64a
Kyle Geske <stungeye@gmail.com>
2022-03-08 09:31:57 -0600

    Created new classes for the C++ Game Mode, End Point and Obstacle.

---
+++ Source/BlockHead/Private/BHEndPoint.cpp
@@ -0,0 +1,27 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "BHEndPoint.h"
+
+// Sets default values
+ABHEndPoint::ABHEndPoint()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+// Called when the game starts or when spawned
+void ABHEndPoint::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void ABHEndPoint::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
